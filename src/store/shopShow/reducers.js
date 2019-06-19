48d6e71e54cd8f6a2a0ed37fe48132ef21cfc3ac
fengwei <fengwei029@foxmail.com>
2019-06-19 08:59:01 +0800

    update

--- src/store/shopShow/reducers.js
+++ src/store/shopShow/reducers.js
@@ -5,31 +5,31 @@ const defaultState = {
     {
       name: "水壶",
       price: "98",
-      picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9DnetgdTyHfaUz_haqpA5dJ61v_DGy-7TEkHb6mMSf7vyfiCrSQ",
+      picture: "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=971896826,2989857190&fm=11&gp=0.jpg",
       count: 5
     },
     {
       name: "抽油烟机",
       price: "2998",
-      picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnQYciDGR5laWL7FN-UVGM2NI6u_gwypspOpVoaZFqblf-M1Qk",
+      picture: "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2697696985,1849698291&fm=11&gp=0.jpg",
       count: 10
     },
     {
       name: "电饭煲",
       price: "498",
-      picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMjQFCb4q1xdK9ZL9k5BFY-_kxeuvajTfPYVrvWb8_q2RkTzKwnA",
+      picture: "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3711819088,2669963969&fm=26&gp=0.jpg",
       count: 115
     },
     {
       name: "冰箱",
       price: "9098",
-      picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWntkdAADQCATBsYCphqPv3T5JlZYoP2orqOF7uHRomohlJSjUBQ",
+      picture: "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3514080566,1376303960&fm=26&gp=0.jpg",
       count: 15
     },
     {
       name: "创维OLED电视",
       price: "98",
-      picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2helgtDJC0xxtoBVQYg6Vb_nZQCfwlRFDC6d2JDcAszryroRd",
+      picture: "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1217386906,936469464&fm=26&gp=0.jpg",
       count: 5
     }
   ],
